6e0cb74c02d1adca8b234c151bd50f603fd0468d
JJ <jj.vega86@gmail.com>
2023-04-13 04:25:40 -0400

    📦 NEW: Add Activity type and Activity process function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
-import { Inter } from "next/font/google";
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from "react";
+type Activity = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  date: string;
+  type: string;
+  totalMiles: number;
+  totalTime: number;
+};
 
-const inter = Inter({ subsets: ["latin"] });
+function processActivities(
+  activities: { [x: string]: any; Date: string | number | Date }[]
+) {
+  let finalActivities: Array<Activity> = [];
+  let index = 0;
+  activities.forEach((activity: { [x: string]: any }) => {
+    finalActivities.push({
+      id: index,
+      firstName: activity["First Name"],
+      lastName: activity["Last Name"],
+      date: activity.Date,
+      type: activity["Activity Type"],
+      totalMiles: activity["Total Miles"],
+      totalTime: activity["Total Time"],
+    });
+    index += 1;
+  });
 
-async function fetchAthletes() {
+  return finalActivities;
+}
+
+async function fetchActivities() {
   try {
     let response = await fetch("https://sheetdb.io/api/v1/3v2zio4yd35vy");
-    let data = await response.json();
-    console.log(data);
+    let json = await response.json();
+    let data = processActivities(json);
     return data;
   } catch (error) {
     console.log(error);
@@ -20,25 +41,15 @@ async function fetchAthletes() {
 }
 
 export default async function Home() {
-  const data = await fetchAthletes();
+  const data = await fetchActivities();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, St Timothy's</h1>
-      {data.map(
-        (athlete: {
-          [x: string]:
-            | string
-            | number
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | ReactFragment
-            | ReactPortal
-            | null
-            | undefined;
-        }) => (
-          <p>{athlete["First Name"]}</p>
-        )
-      )}
+      {data?.map((activity) => (
+        <p key={activity.id}>
+          {activity.firstName} {activity.lastName}
+        </p>
+      ))}
     </main>
   );
 }
